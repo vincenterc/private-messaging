f2f6bac8c9fe35d8bfde268ca115b41daf8d630c
vincenter <vincenterr@yahoo.com.tw>
2024-08-06 20:13:36 +0800

    Integrate Socket.IO

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,17 @@
 import { createServer } from 'http'
+import { Server } from 'socket.io'
 
 const server = createServer()
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3001',
+  },
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+})
+
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () =>
